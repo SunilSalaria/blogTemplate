37a66046393b70ead5d9704f327b62ca2c05259b
Rajeev-Salaria <69579515+Rajeev-Salaria@users.noreply.github.com>
2021-02-10 02:11:52 +0530

    icon and card changes

--- BlogTemplate/wwwroot/js/site.js
+++ BlogTemplate/wwwroot/js/site.js
@@ -1,1 +1,19 @@
 // Write your Javascript code
+$(document).ready(function () {
+
+    $('#eye').hover(function () {
+        // hide password
+        if ($(this).hasClass('fa-eye-slash')) {
+            $(this).removeClass('fa-eye-slash');
+            $(this).addClass('fa-eye');
+            $('.password').attr('type', 'password');
+        }
+        // show password
+        else {
+            $(this).removeClass('fa-eye');
+            $(this).addClass('fa-eye-slash');
+            $('.password').attr('type', 'text');
+           
+        }
+    });
+});
