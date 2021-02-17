9318c400e360c0d14accbbda1ff96c49424868af
Rajeev-Salaria <69579515+Rajeev-Salaria@users.noreply.github.com>
2021-02-18 01:10:42 +0530

    navbar and index page changes

--- BlogTemplate/wwwroot/js/site.js
+++ BlogTemplate/wwwroot/js/site.js
@@ -10,15 +10,28 @@ $(document).ready(function () {
         // hide password on mouse leave    
         $('.password').attr('type', 'password');
     });
+
     // nav link and brand color
     $("#nav-bar .nav-item .nav-link, #nav-bar .navbar-brand, #nav-bar .navbar-toggler span").css({ "color": "white" });
     // toggler button color
-    $("#nav-bar .navbar-toggler").css({ "border": "2px solid white" }); 
+    $("#nav-bar .navbar-toggler").css({ "border": "2px solid white" });
+
+    // navbar color
+    $("#nav-bar .navbar-toggler").click(function () {
+        // navbar background
+        $("#nav-bar .navbar").css({ "background": "white" })
+        // navbar link color
+        $("#nav-bar .nav-link, .navbar-brand, .navbar-toggler span").css({ "color": "black" });
+        // toggler button border
+        $("#nav-bar .navbar-toggler").css({ "border": "2px solid black" });
+    });
+
     });
 
 // on scroll navbar background color change
 $(document).scroll(function () {
-    if ($(document).scrollTop() >= 50) {
+    if ($(document).scrollTop() >= 30) {
+           //navbar background color
         $(".navbar").css({ "background": "white" })
         //font color
         $("#nav-bar .nav-item .nav-link, #nav-bar .navbar-brand, #nav-bar .navbar-toggler span").not(".link-color").css({ "color": "black" });
@@ -28,7 +41,8 @@ $(document).scroll(function () {
         $("#nav-bar .navbar").addClass("border-bottom shadow-sm");
      
     }
-    else if ($(document).scrollTop() <= 50) {
+    else if ($(document).scrollTop() <= 30) {
+           //navbar background color
         $(".navbar").css({ "background": "transparent" });
         //font color
         $("#nav-bar .nav-item .nav-link, #nav-bar .navbar-brand, #nav-bar .navbar-toggler span").css({ "color": "white" });
@@ -36,5 +50,16 @@ $(document).scroll(function () {
         $("#nav-bar .navbar-toggler").css({ "border": "2px solid white" }); 
         // remove navbar and shadow border
         $("#nav-bar .navbar").removeClass("border-bottom shadow-sm");
-    }
+    };
+    // media query for mobile screen 
+    if ($(window).width() < 769) {
+     //navbar background color
+     $(".navbar").css({ "background": "white" });
+     //font color
+     $("#nav-bar .nav-item .nav-link, #nav-bar .navbar-brand, #nav-bar .navbar-toggler span").not(".link-color").css({ "color": "black" });
+     // toggler button border color
+     $("#nav-bar .navbar-toggler").css({ "border": "2px solid black" });
+     // add navbar border and shadow
+     $("#nav-bar .navbar").addClass("border-bottom shadow-sm");
+    };
 });
